c912ad279e4b4fcf08a22b75d98d6cdb783e43d8
ps3869253@gmail.com <ps3869253@gmail.com>
2025-04-14 10:44:44 +0530

    structure of index.js

--- db.js
+++ db.js
@@ -1,5 +1,15 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const ObjectID = mongoose.ObjectID;
 const user = new Schema({
-    
+       name: String,
+       age: Number,
+       email: String,
+       occupation: String 
+})
+
+const Todo = new Schema({
+    task: String,
+    task_status: Boolean,
+    userId :ObjectID
 })
